215ff1ad59b9296358e86da0a8c00283a76668f0
grantcrypto <billionaireknights@gmail.com>
2023-09-08 18:28:27 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 #include <studio.h>
 
 int main() {
-	puts("Programming is like building a multilingual puzzle");
-	return o;
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
