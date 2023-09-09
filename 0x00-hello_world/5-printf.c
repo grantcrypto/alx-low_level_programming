42ec282b0ceba5c95ac047308ec3deb7645df060
grantcrypto <billionaireknights@gmail.com>
2023-09-08 19:25:02 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this refers to the main function oe method
+ *
+ * Return: the value is 0 when condition is true
+**/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
