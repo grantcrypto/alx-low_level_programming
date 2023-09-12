f737bac9c0bff1aebcebff453bd66b19da2506dc
grantcrypto <billionaireknights@gmail.com>
2023-09-11 23:22:02 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	else if (n == 0)
 		printf("Last digit of %d is %d and is 0", n, m);
 	else if (n < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than six and not 0", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
 	printf("\n");
 	return (0);
 }
