3c5c43f3c7a935abb3cf0d2285a384bcf9e943f1
grantcrypto <billionaireknights@gmail.com>
2023-09-14 02:48:10 -0700

    104 fibonacci

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - print n time table
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	while (n >= 0 || n <= 15)
+	{
+		for (i = 0 ; i <= 15 ; i++)
+	{
+		for (j = 0 ; j <= 15 ; j++)
+		
+		result = i * j;
+		{
+		if (j == 0)
+			printf("%d, ", result);
+	}
+		printf("\n");
+		return (void);
+}
+}
+}
+
+
