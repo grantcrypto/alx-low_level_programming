69be3d64be65fd9e14210a200174ac365d522e28
grantcrypto <billionaireknights@gmail.com>
2023-09-13 20:01:00 -0700

    times table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+	int i;
+	for (i = n)
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * times_table - print times table
- * Return: void
+ * Return: 0
  */
 
 void times_table(void)
@@ -12,7 +12,17 @@ void times_table(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
+		{
 			result = i * j;
+			if (j == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d", result);
+				if (j != 9)
+					printf(", ");
+			}
+		}
+	printf("\n");
 	}
-	return ("%d", result);
 }
