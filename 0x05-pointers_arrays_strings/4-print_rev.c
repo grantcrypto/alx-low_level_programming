6de50a15aae6f1beeefbb94c019d52b68bc0fee5
grantcrypto <billionaireknights@gmail.com>
2023-09-18 22:57:56 -0700

    reversed string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - function that print string in revers
+ * @s: string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
