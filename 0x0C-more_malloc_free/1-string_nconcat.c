d8514f7f708177ea8497abf9158d085e8fc371d8
grantcrypto <billionaireknights@gmail.com>
2023-10-08 21:17:58 -0700

    allocate memory using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - function that concatenates two strings while 
+ * second string is an n value
+ * @s1: string one
+ * @s2: string two
+ * @n: number of element to concatenante from s2
+ * Return: pointer to the alllocated memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
