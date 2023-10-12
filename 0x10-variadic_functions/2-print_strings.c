f840df64706d767907d791c1d47a621c618d0360
grantcrypto <billionaireknights@gmail.com>
2023-10-12 15:31:20 -0700

    Write a function that prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,8 +3,8 @@
  * print_strings - prints strings, followed by a new line
  * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
- * 
  */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,5 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything
+ * @format: function that prints anything
+void print_all(const char * const format, ...)
