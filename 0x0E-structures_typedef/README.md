741837059fb21b61797c3fbb4052b2a195cce3fb
grantcrypto <billionaireknights@gmail.com>
2023-10-11 19:27:22 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E-structures_typedef
+Readme for structures-typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that creates a dog info
+ *
+ * @name: name of owner
+ * @age: abe of owner
+ * @owner: the owner of the dog
+ *
+ * Description: first struct with alx
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
