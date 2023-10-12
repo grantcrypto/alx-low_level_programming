bbcf26e5e086a99f6f34a305c2b3c7cb77485dfc
grantcrypto <billionaireknights@gmail.com>
2023-10-12 15:01:37 -0700

    Write a function that prints strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
  * print_strings - prints strings, followed by a new line
  * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
- * Return: void
+ * 
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (str == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
