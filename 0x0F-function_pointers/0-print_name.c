a8fda92a16116462d0174a4e06a36da112a2f60a
grantcrypto <billionaireknights@gmail.com>
2023-10-13 22:06:48 -0700

    Write a function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @f: variable to hold the string name
+ * @name: name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+#include <stddef.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
