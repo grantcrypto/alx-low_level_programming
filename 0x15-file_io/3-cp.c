a8229715c6df9993e3163297c2e43cf154f4f745
grantcrypto <billionaireknights@gmail.com>
2023-11-03 22:56:35 -0700

    Write a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,12 +11,12 @@ void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2])
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 	}
 }
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n" "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv[]);
+	error_file(file_from, file_to, argv);
 
 	numchars = 1024;
 	while (numchars == 1024)
@@ -49,5 +49,22 @@ int main(int argc, char *argv[])
 		numchars = read(file_from, buff, 1024);
 		if (numchars == -1)
 			error_file(-1, 0, argv);
+		numwr = write(file_to, buff, numchars);
+		if (numwr == -1)
+			error_file(0, -1, argv);
 	}
+
+	error_close = close(file_from);
+	if (error_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	error_close = close(file_to);
+	if (error_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
 }
