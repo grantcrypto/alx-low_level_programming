0f900fbd7e7efe2eb0940e699d3c6844c6b60cd4
grantcrypto <billionaireknights@gmail.com>
2023-11-03 19:43:44 -0700

    Create a function that creates a file and write into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file -  function that creates a file
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int numletters;
+	int retwr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (numletters = 0; text_content[numletters]; numletters++)
+		;
+	retwr = write(fd, text_content, numletters);
+
+	if (retwr == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
