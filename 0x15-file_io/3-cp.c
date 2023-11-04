c1a67acf1c35db7077772c8e723a6b42fcc4c4d0
grantcrypto <billionaireknights@gmail.com>
2023-11-03 22:00:19 -0700

    Write a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * error_file - check if file is open and handle errors
+ * @file_from: initial file to copy from and to check
+ * @file_to: destination file to copy to
+ * @argv: argument vector
+ * Return: void
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+			exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2])
+			exit(99);
+	}
+}
+
+/**
+ * main - copy content of a file from one to another
+ * @argc: argument count
+ * @argv: argument vectoer
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, error_close;
+	ssize_t numchars, numwr;
+	char buff[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n" "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv[]);
+
+	numchars = 1024;
+	while (numchars == 1024)
+	{
+		numchars = read(file_from, buff, 1024);
+		if (numchars == -1)
+			error_file(-1, 0, argv);
+	}
+}
